18027af4857c6e6498fa0d55494fbac56c2da2ff
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2022-03-02 09:29:06 +0100

    Add test class for the bucket management

---
+++ flaskr/aws_bucket_manager.py
@@ -0,0 +1,15 @@
+class AwsBucketManager():
+    def __init__(self, bucket_url) -> None:
+        self.bucket_url = bucket_url
+
+    def create_object():
+        pass
+
+    def object_exists():
+        pass
+
+    def download_object():
+        pass
+
+    def remove_object():
+        pass
