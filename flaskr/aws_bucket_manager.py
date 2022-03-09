7df1d4f86ff3d25f323cad613d31e8fdbe9ff1ef
OLIVEIRA-RAMOS Dylan <Dylan.OLIVEIRA-RAMOS@cpnv.ch>
2022-03-09 09:36:07 +0100

    Be able to upload a file

--- flaskr/aws_bucket_manager.py
+++ flaskr/aws_bucket_manager.py
@@ -1,15 +1,31 @@
-class AwsBucketManager():
-    def __init__(self, bucket_url) -> None:
-        self.bucket_url = bucket_url
+import boto3
+import asyncio
 
-    def create_object():
+
+class AwsBucketManager:
+    """
+    Aws Bucket Manager using s3 resource
+    Useful link : https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html#object
+    """
+
+    def __init__(self) -> None:
+        self.s3 = boto3.resource('s3')
+
+    async def create_object(self, bucket_url, file_name):
+        try:
+            key = file_name.split('/')[-1]
+            self.s3.Bucket(bucket_url).upload_file(file_name, key)
+        except:
+            return 'An error occured.', 400
+
+        return 'The file has been uploaded.', 201
+
+
+    def object_exists(self):
         pass
 
-    def object_exists():
+    def download_object(self):
         pass
 
-    def download_object():
-        pass
-
-    def remove_object():
+    def remove_object(self):
         pass
