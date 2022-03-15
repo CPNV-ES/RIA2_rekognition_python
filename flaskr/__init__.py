4f68648e926d6afdcdeb193a0888e9d841f443f9
Quentin Neves <neves.quentin@gmail.com>
2022-03-15 11:14:41 +0100

    Describe swagger

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -15,7 +15,7 @@ from werkzeug.utils import secure_filename
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
-    api = Api(app)
+    api = Api(app, "1.0", "Image rekognition", "An API to analyse images on AWS Rekognise and get a SQL file from restults")
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
