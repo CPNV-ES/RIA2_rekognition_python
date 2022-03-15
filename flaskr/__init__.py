0f865b8e5206ca1952f2fd01a369fde91cf6905c
Quentin Neves <neves.quentin@gmail.com>
2022-03-15 10:56:31 +0100

    Implement Swagger

--- flaskr/__init__.py
+++ flaskr/__init__.py
@@ -6,11 +6,13 @@ import re
 from flaskr.aws_bucket_manager import AwsBucketManager
 import datetime
 import os
+from flask_restx import Api, Resource
 
 
 def create_app(test_config=None):
     # create and configure the app
     app = Flask(__name__, instance_relative_config=True)
+    api = Api(app)
     app.config.from_mapping(
         SECRET_KEY='dev',
         DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
@@ -30,62 +32,65 @@ def create_app(test_config=None):
         os.makedirs(app.instance_path)
     except OSError:
         pass
-
+    
     # a simple page that says hello
-    @app.route('/hello')
-    def hello():
-        return 'Hello, World!'
+    @api.route('/hello', endpoint='hello')
+    class HelloWorld(Resource):
+        def hello():
+            return 'Hello, World!'
 
-    @app.route('/upload', methods=['POST'])
-    async def upload():
-        result = await aws_bucket_manager.create_object(
-            os.getenv('BUCKET_URL'), './monkey.jpg')
+    @api.route('/upload', methods=['POST'], endpoint='upload')
+    class Image(Resource):
+        async def post():
+            result = await aws_bucket_manager.create_object(
+                os.getenv('BUCKET_URL'), './monkey.jpg')
 
-        return result
+            return result
 
-    @app.route('/api/generate/sql', methods=['POST'])
-    async def generate_sql():
-        content = request.get_json(silent=True)
+    @api.route('/api/generate/sql', methods=['POST'])
+    class SQL(Resource):
+        async def post():
+            content = request.get_json(silent=True)
 
-        try:
-            if (not content):
-                raise Exception("No content")
-            df = pd.DataFrame(js.loads(js.dumps(content)))
-            sql_text = get_insert_query_from_df(df, 'rekognition_result')
+            try:
+                if (not content):
+                    raise Exception("No content")
+                df = pd.DataFrame(js.loads(js.dumps(content)))
+                sql_text = get_insert_query_from_df(df, 'rekognition_result')
 
-            tmp = tempfile.NamedTemporaryFile(delete=False)
+                tmp = tempfile.NamedTemporaryFile(delete=False)
 
-            tmp.write(bytes(sql_text, 'utf-8'))
-            generate_sql_filename = datetime.datetime.now().strftime(
-                "%Y-%m-%d_%H-%M-%S-%f") + "_MAAAAA.sql"
-            object_url = '%s/sql/%s' % (os.getenv("BUCKET_URL"),
-                                        generate_sql_filename)
-            if(os.path.exists(tmp.name)):
-                bucket = AwsBucketManager()
-                await bucket.create_object(object_url, tmp.name)
+                tmp.write(bytes(sql_text, 'utf-8'))
+                generate_sql_filename = datetime.datetime.now().strftime(
+                    "%Y-%m-%d_%H-%M-%S-%f") + "_MAAAAA.sql"
+                object_url = '%s/sql/%s' % (os.getenv("BUCKET_URL"),
+                                            generate_sql_filename)
+                if(os.path.exists(tmp.name)):
+                    bucket = AwsBucketManager()
+                    await bucket.create_object(object_url, tmp.name)
 
-            tmp.close()
-            message = object_url
-        except Exception as e:
-            message = str(e)
+                tmp.close()
+                message = object_url
+            except Exception as e:
+                message = str(e)
 
-        return jsonify({"message": message})
+            return jsonify({"message": message})
 
-    def get_insert_query_from_df(df, dest_table):
-        insert = """
-        INSERT INTO `{dest_table}` (
-            """.format(dest_table=dest_table)
+        def get_insert_query_from_df(df, dest_table):
+            insert = """
+            INSERT INTO `{dest_table}` (
+                """.format(dest_table=dest_table)
 
-        columns_string = str(list(df.columns))[1:-1]
-        columns_string = re.sub(r' ', '\n        ', columns_string)
-        columns_string = re.sub(r'\'', '', columns_string)
+            columns_string = str(list(df.columns))[1:-1]
+            columns_string = re.sub(r' ', '\n        ', columns_string)
+            columns_string = re.sub(r'\'', '', columns_string)
 
-        values_string = ''
+            values_string = ''
 
-        for row in df.itertuples(index=False, name=None):
-            values_string += re.sub(r'nan', 'null', str(row))
-            values_string += ',\n'
+            for row in df.itertuples(index=False, name=None):
+                values_string += re.sub(r'nan', 'null', str(row))
+                values_string += ',\n'
 
-        return insert + columns_string + ')\n     VALUES\n' + values_string[:-2] + ';'
+            return insert + columns_string + ')\n     VALUES\n' + values_string[:-2] + ';'
 
     return app
