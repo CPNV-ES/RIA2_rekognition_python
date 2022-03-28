a2982e0e5ade1426e340bdb26abd822b836c8ea1
Mathieu.Burnat@cpnv.ch <Mathieu.Burnat@cpnv.ch>
2022-03-28 14:19:19 +0200

    Refactor rekognition_object and display the confidence number

--- flaskr/rekognition_objects.py
+++ flaskr/rekognition_objects.py
@@ -74,14 +74,14 @@ class RekognitionFace:
             self.age_range = (age_range.get('Low'), age_range.get('High'))
         else:
             self.age_range = None
-        self.smile = face.get('Smile', {}).get('Value')
-        self.eyeglasses = face.get('Eyeglasses', {}).get('Value')
-        self.sunglasses = face.get('Sunglasses', {}).get('Value')
-        self.gender = face.get('Gender', {}).get('Value', None)
-        self.beard = face.get('Beard', {}).get('Value')
-        self.mustache = face.get('Mustache', {}).get('Value')
-        self.eyes_open = face.get('EyesOpen', {}).get('Value')
-        self.mouth_open = face.get('MouthOpen', {}).get('Value')
+        self.smile = face.get('Smile')
+        self.eyeglasses = face.get('Eyeglasses')
+        self.sunglasses = face.get('Sunglasses')
+        self.gender = face.get('Gender')
+        self.beard = face.get('Beard')
+        self.mustache = face.get('Mustache')
+        self.eyes_open = face.get('EyesOpen')
+        self.mouth_open = face.get('MouthOpen')
         self.emotions = [
             emo.get('Type') for emo in face.get('Emotions', [])
             if emo.get('Confidence', 0) > 50
@@ -111,6 +111,34 @@ class RekognitionFace:
             rendering['image_id'] = self.image_id
         if self.timestamp is not None:
             rendering['timestamp'] = self.timestamp
+        
+        if self.smile is not None:
+            rendering['smile'] = self.smile
+
+        if self.eyeglasses is not None:
+            rendering['eyeglasses'] = self.eyeglasses
+        
+        if self.sunglasses is not None:
+            rendering['sunglasses'] = self.sunglasses
+        
+        if self.beard is not None:
+            rendering['beard'] = self.beard
+
+        if self.mustache is not None:
+            rendering['mustache'] = self.mustache
+        
+        if self.eyes_open is not None:
+            rendering['eyes_open'] = self.eyes_open
+
+        if self.mouth_open is not None:
+            rendering['mouth_open'] = self.mouth_open
+        
+        if self.pose is not None:
+            rendering['pose'] = self.pose
+        
+        if self.quality is not None:
+            rendering['quality'] = self.quality
+
         has = []
         if self.smile:
             has.append('smile')
