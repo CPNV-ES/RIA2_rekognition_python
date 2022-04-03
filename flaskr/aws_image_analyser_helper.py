2d4d0c40588349185acb640080176e137bfd211a
Quentin Neves <neves.quentin@gmail.com>
2022-04-03 18:41:30 +0200

    Document

--- flaskr/aws_image_analyser_helper.py
+++ flaskr/aws_image_analyser_helper.py
@@ -2,9 +2,15 @@ import string
 
 class IAwsImageAnalyserHelper:
     """ 
-    ImageAnalayserHelper
+    This interface defines what parameters are required to analyse faces on a picture
     """
-    def MakeAnalysisRequest(url: string, maxLabels: int, minConfidence: int): raise NotImplementedError
+
+    
+    def MakeAnalysisRequest(url: string, maxLabels: int, minConfidence: int): 
+        """
+        Make a requests to Amazon's rekognition to analyse faces on a picture with specified parameters and return a json
+        """
+        raise NotImplementedError
 
 class AwsImageAnalyserHelper(IAwsImageAnalyserHelper):
 
