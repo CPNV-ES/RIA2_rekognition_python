5a7e376869c4b91c27dd90c87f26ddfcc91d4db4
Quentin Neves <neves.quentin@gmail.com>
2022-04-03 18:34:08 +0200

    Create interface and implement it

--- flaskr/aws_image_analyser_helper.py
+++ flaskr/aws_image_analyser_helper.py
@@ -1,6 +1,12 @@
-class AwsImageAnalyserHelper:
+import string
+
+class IAwsImageAnalyserHelper:
     """ 
     ImageAnalayserHelper
     """
-    def MakeAnalysisRequest(url):
-        return True
+    def MakeAnalysisRequest(url: string, maxLabels: int, minConfidence: int): raise NotImplementedError
+
+class AwsImageAnalyserHelper(IAwsImageAnalyserHelper):
+
+    def MakeAnalysisRequest(url: string, maxLabels: int, minConfidence: int):
+        return "oui"
