91be40172745ff75c48628e4651e25ba16e124a8
Quentin Neves <neves.quentin@gmail.com>
2022-04-03 17:28:12 +0200

    Basic test with local file only
    URL from bucket's objects isn't available yet

--- tests/test_image_analyser_helper.py
+++ tests/test_image_analyser_helper.py
@@ -1,8 +1,8 @@
+from re import S
+from flaskr.rekognition_image_detection import face_from_local_file, face_from_url
 import unittest
 import os
-
-from flaskr.aws_image_analyser_helper import AwsImageAnalyserHelper
-
+import shutil
 
 class ImageAnalyserHelperTestCase(unittest.IsolatedAsyncioTestCase):
     """
@@ -14,18 +14,56 @@ class ImageAnalyserHelperTestCase(unittest.IsolatedAsyncioTestCase):
         This test method initializes the context before each test method run.
         """
         self.bucket_name = os.getenv('BUCKET_NAME')
-        self.image_name = "pexels-pixabay-53370.jpg"
-        self.file = open("./tests/" + self.image_name, "rb")
-        self.helper = AwsImageAnalyserHelper()
-        self.json_name = "expected_pexels-pixabay-53370.json"
-        self.full_path_to_json = "./tests/" + self.json_name
+
+        # Prepare picutre
+        self.image_name = "test.jpg"
+        shutil.copyfile(self.image_name, "./tests/" + self.image_name)
+        self.picture = open("./tests/" + self.image_name, "rb")
+
+        # Prepare expected json
+        # Here instead of using an outdated json we get it fresh so we make sure there's no difference
+        # Bonus : We don't need to manually update it
+        self.expected_json = face_from_local_file(self.image_name)
+
+        #self.json_name = "expected_test.json"
+        #full_path_to_json = "./tests/" + self.json_name
+        #file = open(full_path_to_json, "r")
+        # file.close()
+
+        # Query args
         self.max_labels = 1
 
+    def tearDown(self):
+        self.picture.close()
+
+    """    
+    This test method checks the MakeAnalysis'answer
+    when we try to analyze a local file
+    """
     async def test_analyse_localfile_success(self):
-        actual_json = ""
-        expected_json = open(self.full_path_to_json, "r").read()
-        
-        self.assertFalse(False)
+
+        actual_json = face_from_local_file(self.image_name)
+
+        self.assertEqual(self.expected_json, actual_json)
+
+    """    
+    This test method checks the MakeAnalysis'answer
+    when we try to analyze a data object presents on a bucket
+    """
+    async def test_analyse_dataobject_succes(self):
+        actual_json = face_from_local_file(self.image_name)
+
+        self.assertEqual(self.expected_json, actual_json)
+
+    """    
+    This test method checks the MakeAnalysis'answer
+    when we try to analyze a data object presents on a bucket
+    """
+    async def test_to_json_dataobject_succes(self):
+        actual_json = face_from_local_file(self.image_name)
+
+        self.assertEqual(self.expected_json, actual_json)
+
 
 if __name__ == '__main__':
     unittest.main()
