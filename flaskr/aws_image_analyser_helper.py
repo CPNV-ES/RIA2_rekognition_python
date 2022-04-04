28d20f968e23c958f6b15119d09a0a9e915fb991
Quentin Neves <neves.quentin@gmail.com>
2022-04-04 10:04:38 +0200

    Implement correct inferface

--- flaskr/aws_image_analyser_helper.py
+++ flaskr/aws_image_analyser_helper.py
@@ -1,11 +1,11 @@
 import string
+from abc import ABC, abstractclassmethod
 
-class IAwsImageAnalyserHelper:
+class IAwsImageAnalyserHelper(ABC):
     """ 
     This interface defines what parameters are required to analyse faces on a picture
     """
-
-    
+    @abstractclassmethod
     def MakeAnalysisRequest(url: string, maxLabels: int, minConfidence: int): 
         """
         Make a requests to Amazon's rekognition to analyse faces on a picture with specified parameters and return a json
@@ -13,6 +13,5 @@ class IAwsImageAnalyserHelper:
         raise NotImplementedError
 
 class AwsImageAnalyserHelper(IAwsImageAnalyserHelper):
-
-    def MakeAnalysisRequest(url: string, maxLabels: int, minConfidence: int):
-        return "oui"
+    def MakeAnalysisRequest(url: string, maxLables: int, minConfidence: int):
+        return True
