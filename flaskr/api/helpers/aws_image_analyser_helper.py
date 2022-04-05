5983568983133bcc60c74d4ca8073294040837e9
Quentin Neves <neves.quentin@gmail.com>
2022-04-05 19:06:45 +0200

    Separated interface from its implementation

---
+++ flaskr/api/helpers/aws_image_analyser_helper.py
@@ -0,0 +1,15 @@
+import os
+from flaskr.api.interfaces.i_image_analyser_helper import IAwsImageAnalyserHelper
+from flaskr.api.managers.aws_bucket_manager import AwsBucketManager
+from flaskr.api.managers.rekognition_image_detection import face_from_url
+
+class AwsImageAnalyserHelper(IAwsImageAnalyserHelper):
+
+    def __init(self):
+        self.aws_bucket_manager = AwsBucketManager()
+        self.bucket_name = os.getenv('BUCKET_NAME')
+
+    async def MakeAnalysisRequest(self, url: str, maxLables: int, minConfidence: int):
+        result = await self.aws_bucket_manager.create_object(self.bucket_name, url)
+        
+        return face_from_url(result[0], False)
--- flaskr/api/interfaces/i_image_analyser_helper.py
+++ flaskr/api/interfaces/i_image_analyser_helper.py
@@ -1,17 +1,11 @@
-import string
 from abc import ABC, abstractclassmethod
-
 class IAwsImageAnalyserHelper(ABC):
     """ 
     This interface defines what parameters are required to analyse faces on a picture
     """
     @abstractclassmethod
-    def MakeAnalysisRequest(url: string, maxLabels: int, minConfidence: int): 
+    def MakeAnalysisRequest(url: str, maxLabels: int, minConfidence: int):
         """
         Make a requests to Amazon's rekognition to analyse faces on a picture with specified parameters and return a json
         """
         raise NotImplementedError
-
-class AwsImageAnalyserHelper(IAwsImageAnalyserHelper):
-    def MakeAnalysisRequest(url: string, maxLables: int, minConfidence: int):
-        return True
