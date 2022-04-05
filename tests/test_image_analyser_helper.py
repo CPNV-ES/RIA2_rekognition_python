87aba61047ccdb35ddb651bd626ca6970c112a65
Quentin Neves <neves.quentin@gmail.com>
2022-04-05 19:12:17 +0200

    Update test to use implementation of helper interface

--- tests/test_image_analyser_helper.py
+++ tests/test_image_analyser_helper.py
@@ -1,8 +1,6 @@
-from ast import dump
-import json
-from re import S
 from flaskr.api.managers.rekognition_image_detection import face_from_local_file, face_from_url
 from flaskr.api.managers.aws_bucket_manager import AwsBucketManager
+from flaskr.api.helpers.aws_image_analyser_helper import AwsImageAnalyserHelper
 import unittest
 import os
 import shutil
@@ -26,8 +24,9 @@ class ImageAnalyserHelperTestCase(unittest.IsolatedAsyncioTestCase):
         # os.remove(self.local_image_path)
         shutil.copyfile(self.local_image_name, self.image_copy_path)
 
-        # Upload picture
+        # Prepare helpers
         self.aws_bucket_manager = AwsBucketManager()
+        self.image_analyser_helper = AwsImageAnalyserHelper()
 
         # Prepare expected json
         self.expected_json = face_from_local_file(self.local_image_name)
@@ -47,7 +46,7 @@ class ImageAnalyserHelperTestCase(unittest.IsolatedAsyncioTestCase):
         # When
         actual_json = face_from_local_file(picture)
 
-        #Then
+        # Then
         self.assertEqual(self.expected_json, actual_json)
 
     """    
@@ -56,10 +55,11 @@ class ImageAnalyserHelperTestCase(unittest.IsolatedAsyncioTestCase):
     """
     async def test_analyse_dataobject_succes(self):
         # Given
-        result = await self.aws_bucket_manager.create_object(self.bucket_name, self.image_copy_path)
-        
+        picture = self.image_copy_path
+
         # When
-        actual_json = face_from_url(result[0], False)
+        actual_json = self.image_analyser_helper.MakeAnalysisRequest(
+            self.image_copy_path)
 
         # Then
         self.assertEqual(self.expected_json, actual_json)
@@ -70,10 +70,11 @@ class ImageAnalyserHelperTestCase(unittest.IsolatedAsyncioTestCase):
     """
     async def test_to_json_dataobject_succes(self):
         # Given
-        result = await self.aws_bucket_manager.create_object(self.bucket_name, self.image_copy_path)
-        
+        picture = self.image_copy_path
+
         # When
-        actual_json = face_from_url(result[0], False)
+        actual_json = self.image_analyser_helper.MakeAnalysisRequest(
+            self.image_copy_path)
 
         # Then
         self.assertEqual(self.expected_json, actual_json)
